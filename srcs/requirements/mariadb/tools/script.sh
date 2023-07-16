ad036e5c6d3a36ccbdc6b8bc46c5c47686af95c1
Brahim Boulhanna <bboulhan@e3r2p15.1337.ma>
2023-07-16 22:53:40 +0100

    mariadb is good

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -7,4 +7,6 @@ mysql < /ibra/init.sql
 
 # sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# kill $(cat /var/run/mysqld/mysqld.pid)
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
