c09cfb47fba5851a8fcf48d9b4c148dfc8a18720
Brahim Boulhanna <bboulhan@e3r10p4.1337.ma>
2023-07-17 19:35:32 +0100

    fix nginx problem

--- srcs/requirements/mariadb/tools/init.sql
+++ srcs/requirements/mariadb/tools/init.sql
@@ -1,3 +1,15 @@
+-- SET @data_name = '${DB_NAME}';
+-- SET @data_user = '${USER_NAME}';
+-- SET @data_pass = '${USER_PASS}';
+-- SET @data_root = '${ROOT_PASS}';
+
+
+-- CREATE DATABASE IF NOT EXISTS @data_name;
+-- CREATE USER IF NOT EXISTS '@data_user'@'%' IDENTIFIED BY '@data_pass';
+-- GRANT ALL ON @data_name.* TO '@data_user'@'%';
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY '@data_root';
+-- FLUSH PRIVILEGES;
+
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASS';
 GRANT ALL ON $DB_NAME.* TO '$USER_NAME'@'%';
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,17 @@
 service mysql start
 
-sleep 4
+# DB_NAME=IBRA_DB
+# USER_NAME=BBOULHAN
+# USER_PASS=ibra2001
+# ROOT_PASS=root42
+# sleep 4
+sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql < /ibra/init.sql
-
-
-# sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -e "CREATE USER IF NOT EXISTS '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASS';"
+mysql -e "GRANT ALL ON $DB_NAME.* TO '$USER_NAME'@'%';"
+mysql -e "ALTER USER 'ROOT'@'localhost' IDENTIFIED BY '$ROOT_PASS';"
+mysql -e "FLUSH PRIVILEGES;"
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
