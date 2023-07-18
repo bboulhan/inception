18daae584f330d6d06270e6ecd04a8b964a8cf5a
Brahim Boulhanna <bboulhan@e3r9p8.1337.ma>
2023-07-18 21:46:35 +0100

    volumes

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,17 +1,15 @@
 service mysql start
 
-# DB_NAME=IBRA_DB
-# USER_NAME=BBOULHAN
-# USER_PASS=ibra2001
-# ROOT_PASS=root42
 # sleep 4
-sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASS';"
 mysql -e "GRANT ALL ON $DB_NAME.* TO '$USER_NAME'@'%';"
 mysql -e "ALTER USER 'ROOT'@'localhost' IDENTIFIED BY '$ROOT_PASS';"
 mysql -e "FLUSH PRIVILEGES;"
+
+
+sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
