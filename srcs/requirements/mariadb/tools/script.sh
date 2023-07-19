62361110e448dd78b8a82fd6c9bf978e9ec35777
Brahim Boulhanna <bboulhan@e3r4p15.1337.ma>
2023-07-19 19:45:52 +0100

    remove sql script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,18 @@
-service mysql start
+#!/bin/sh
+sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+service mysql start
 # sleep 4
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASS';"
-mysql -e "GRANT ALL ON $DB_NAME.* TO '$USER_NAME'@'%';"
-mysql -e "ALTER USER 'ROOT'@'localhost' IDENTIFIED BY '$ROOT_PASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS';"
 mysql -e "FLUSH PRIVILEGES;"
 
 
-sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-
 kill $(cat /var/run/mysqld/mysqld.pid)
+
+echo "restart"
 
 mysqld
