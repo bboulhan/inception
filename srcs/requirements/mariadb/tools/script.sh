2c6987af95bb12c887957514291b09441854db1b
Brahim Boulhanna <bboulhan@e3r3p17.1337.ma>
2023-07-22 06:27:19 +0100

    done with wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,7 +3,7 @@ sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
-# sleep 10
+sleep 5
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASS';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'%';"
@@ -12,7 +12,5 @@ mysql -e "FLUSH PRIVILEGES;"
 
 
 kill $(cat /var/run/mysqld/mysqld.pid)
-
-echo "restart"
 
 mysqld 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-echo "hello ibra"
-echo "hello ibra"
-
 mkdir -p /run/php
 touch /run/php/php7.3-fpm.pid
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
@@ -13,53 +9,19 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar #download wp-cli (WordPress Command Line Interface pour managing your site)
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp # you might use the wp plugin install command to install a plugin, or the wp theme list command to list the installed themes.
+
 	cd /var/www/html
 	wp core download --allow-root #download the latest version of WordPress  and The --allow-root option allows the command to be executed by the root user
-	touch /var/www/html/wp-config.php
+	
 	sed -i "s/database_name_here/${DB_NAME}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/username_here/${DB_USER}/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/password_here/${DB_PSSWRD}/g" "/var/www/html/wp-config-sample.php"
+	sed -i "s/username_here/${USER_NAME}/g" "/var/www/html/wp-config-sample.php"
+	sed -i "s/password_here/${USER_PASS}/g" "/var/www/html/wp-config-sample.php"
 	sed -i "s/localhost/${DB_HOST}/g" "/var/www/html/wp-config-sample.php"
+	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+    
+	wp core install --url=localhost/ --title=bboulhan --admin_user=$USER_NAME --admin_password=$USER_PASS --admin_email=$ADMIN_EMAIL --skip-email  --path="/var/www/html" --allow-root
+    wp user create $WP_USER $USER_EMAIL --role=author --user_pass=$WP_PASS --allow-root
 
 fi
 
-
-# sleep 4
-# curl -O https://wordpress.org/latest.tar.gz && tar -xvzf latest.tar.gz --strip-components=1 && rm latest.tar.gz
-
-# chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html
-# wp core download --path="/var/www/html" --allow-root
-echo "hello ibra 2"
-echo "hello ibra 2"
-
 php-fpm7.3 -F
-# service php7.3-fpm start
-
-
-# 	mkdir -p /run/php/;
-# 	touch /run/php/php7.3-fpm.pid; #Store PID files for PHP processes managed by the PHP-FPM
-# 	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
-#  if [ ! -f /var/www/html/wp-config.php ]; then
-# 	mkdir -p /var/www/html
-# 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar; #download wp-cli (WordPress Command Line Interface pour managing your site)
-# 	chmod +x wp-cli.phar; 
-# 	mv wp-cli.phar /usr/local/bin/wp; # you might use the wp plugin install command to install a plugin, or the wp theme list command to list the installed themes.
-# 	cd /var/www/html;
-# 	wp core download --allow-root; #download the latest version of WordPress  and The --allow-root option allows the command to be executed by the root user
-# 	touch /var/www/html/wp-config.php;
-# 	sed -i "s/database_name_here/${DB_NAME}/g" "/var/www/html/wp-config-sample.php"
-# 	sed -i "s/username_here/${DB_USER}/g" "/var/www/html/wp-config-sample.php"
-# 	sed -i "s/password_here/${DB_PSSWRD}/g" "/var/www/html/wp-config-sample.php"
-# 	sed -i "s/localhost/${DB_HOST}/g" "/var/www/html/wp-config-sample.php"
-# 	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php;
-# 	# install WordPress using the specified options
-# 	wp core install --allow-root --url=$URL --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PSSWRD --admin_email=$ADMIN_EMAIL
-# 	wp user create --allow-root ${DB_USER} ${USER_EMAIL} --user_pass=${DB_PSSWRD}; # create a new user for your WordPress site
-#     wp plugin install  --allow-root redis-cache --activate
-#    	wp redis  --allow-root enable
-# 	wp config set WP_REDIS_HOST ${REDIS_HOST} --allow-root
-# 	wp config set WP_REDIS_PORT ${REDIS_PORT} --allow-root
-# 	wp config set WP_CACHE ${REDIS_CACHE} --allow-root
-# 	echo "Installation successful !"
-#  fi
-# exec "$@"
